017b442255fb6bf2b36d26f68e11709de4ca02d7
Vrushabh-Kun-Dev <vrushabh.kun.dev@gmail.com>
2026-02-08 17:41:54 +0530

    React Router Dom

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import './Navbar.css'
+
+
+const Navbar = () => {
+    return (
+        <>
+        <div className="navbar">
+            <Link to={'/'}>Home</Link>
+            <Link to={'/about'}>About</Link>
+            <Link to={'/contact'}>Contact</Link>
+
+        </div>
+        </>
+    )
+}
+
+
+export default Navbar;
